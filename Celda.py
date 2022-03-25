13faa235285fc087f05654d8609214aed13fe7a3
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2022-03-25 08:10:02 -0600

    celda

---
+++ Celda.py
@@ -0,0 +1,53 @@
+class Celda:
+    def __init__(self,i,j,caracter,paso,fin):
+        self.setI(i)
+        self.setJ(j)
+        self.setCaracter(caracter)
+        self.setPaso(paso)
+        self.setFin(fin)
+        self.setVisitado(False)
+
+    def getCaracter(self):
+        return self.caracter
+
+    def setCaracter(self,caracter):
+        self.caracter = caracter
+    
+    def getI(self):
+        return self.i
+    
+    def setI(self,i):
+        self.i = i
+
+    def getJ(self):
+        return self.posY
+    
+    def setJ(self,posY):
+        self.posY = posY
+    
+    def arribaDisponible(self):
+        return self.paso.get(0)
+
+    def derechaDisponible(self):
+        return self.paso.get(1)
+
+    def abajoDisponible(self):
+        return self.paso.get(2)
+
+    def izquierdaDisponible(self):
+        return self.paso.get(3)
+
+    def setPaso(self,paso):
+        self.paso = paso
+    
+    def isFin(self):
+        return self.fin
+    
+    def setFin(self,fin):
+        self.fin = fin
+    
+    def isVisitado(self):
+        return self.visitado
+    
+    def setVisitado(self,visitado):
+        self.visitado = visitado
